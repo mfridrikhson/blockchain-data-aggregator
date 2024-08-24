8db6f5d4f5a3cb83c23a13a1e5f10bd368f33697
Michael Fridrikhson <fridrihson.1701@gmail.com>
2024-08-24 20:56:19 +0300

    Populate main with application logic

--- rates/main.go
+++ rates/main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log/slog"
+	"time"
+)
 
+func main() {
+	container := NewContainer()
+	defer container.StorageProvider.Close()
+
+	slog.Info("running exchange rates ingestion", slog.Time("date", container.Date))
+
+	rawData, err := container.DataFetcher.GetRatesData(container.Date)
+	if err != nil {
+		panic(err)
+	}
+
+	transformedData, err := container.DataTransformer.Transform(rawData)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(transformedData))
+
+	fileName := getFileName(container.Date, "json")
+	err = container.StorageProvider.UploadToStorage(transformedData, fileName)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getFileName(date time.Time, format string) string {
+	dateString := date.Format(time.DateOnly)
+
+	directory := "raw/exchange-rates"
+
+	return fmt.Sprintf("%s/%s.%s", directory, dateString, format)
 }
